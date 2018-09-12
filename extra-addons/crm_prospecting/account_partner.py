193f67c372ec1613abb01d46b5c7f1de0db6dca3
ELHAMDANY <elhamdanyaziz@gmail.com>
2018-09-12 01:03:04 +0100

    add new correctif 120918

---
+++ extra-addons/crm_prospecting/account_partner.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+
+class res_partner(osv.osv):
+
+    _inherit = 'res.partner'
+    _description = 'Partner'
+
+    _columns = {
+        'property_account_payable': fields.property(
+            type='many2one',
+            relation='account.account',
+            string="Account Payable",
+            domain="[('type', '=', 'payable')]",
+            help="This account will be used instead of the default one as the payable account for the current partner",
+            required=False),
+        'property_account_receivable': fields.property(
+            type='many2one',
+            relation='account.account',
+            string="Account Receivable",
+            domain="[('type', '=', 'receivable')]",
+            help="This account will be used instead of the default one as the receivable account for the current partner",
+            required=False),
+    }
+
+    _defaults = {
+        'property_account_payable': 11609,
+        'property_account_receivable': 15902
+    }
+
